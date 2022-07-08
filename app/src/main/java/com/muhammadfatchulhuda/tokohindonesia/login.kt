72764bc15d67eaa53f14dceffe983cc0d4838242
fatchulhuda88 <muhammadfatchulhuda88@gmail.com>
2022-07-08 19:39:22 +0700

    Recyclerview app

---
+++ app/src/main/java/com/muhammadfatchulhuda/tokohindonesia/login.kt
@@ -0,0 +1,24 @@
+package com.muhammadfatchulhuda.tokohindonesia
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.muhammadfatchulhuda.tokohindonesia.databinding.ActivityLoginBinding
+private lateinit var binding: ActivityLoginBinding
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //inisialisasi binding
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        //memanggil layout dengan menggunakkan binding.root
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            val dataLogin=ModelLogin(binding.txtUserName.text.toString(),binding.txtPassword.text.toString())
+            val intent = Intent(this,MainActivity::class.java)
+            intent.putExtra("data",dataLogin)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
